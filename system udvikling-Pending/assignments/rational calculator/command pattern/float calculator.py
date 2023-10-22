6f16c80337849f7f8d21aa1fbf5c29494ff0ccc8
zimula <zimula@live.dk>
2023-10-23 00:32:58 +0200

    float calculator with design pattern finished

--- system udvikling-Pending/assignments/rational calculator/command pattern/float calculator.py
+++ system udvikling-Pending/assignments/rational calculator/command pattern/float calculator.py
@@ -8,6 +8,18 @@ class command_add(Command):
     def execute(self, x, y):
         return x + y
 
+class command_subtract(Command):
+    def  execute(self, x, y):
+        return x - y
+
+class command_multiply(Command):
+    def execute(self, x, y):
+        return x * y
+
+class command_divide(Command):
+    def execute(self, x, y):
+        return x / y
+
 'command invoker'
 class Calculator:
     def __init__(self):
@@ -23,18 +35,27 @@ class Calculator:
             return self.command.execute(x,y)
         else: 
             return 'no command set'
+        
+'Main (should probably put everything in its own module) '
+def main():
+    calculator = Calculator()
+    while True:
+        x = float(input("Enter x value: "))
+        y = float(input("Enter y value: "))
+        operation = input("Enter operation: ")
 
-'Main'
-calculator = Calculator()
-calculator.set_command(command_add())
-
-while True:
-    x = float(input("Enter x value: "))
-    y = float(input("Enter y value"))
-    operation = input("Enter operation: ")
-    if operation not in ['+', '-', '*', '/']:
+        if operation not in ['+', '-', '*', '/']:
+            break
+        elif operation == '+':
+            calculator.set_command(command_add())
+        elif operation == '-':
+            calculator.set_command(command_subtract())
+        elif operation == '*':
+            calculator.set_command(command_multiply())
+        elif operation == '/':
+            calculator.set_command(command_divide())
+        result = round(calculator.calculate(x,y),2)
+        print(result)
         break
-    elif operation == '+':
-        result = calculator.calculate(x,y)
-    print(result)
-    break
+if __name__ == '__main__':
+    main()
--- system udvikling-Pending/assignments/rational calculator/readme.md
+++ system udvikling-Pending/assignments/rational calculator/readme.md
@@ -1,3 +1,14 @@
-1. create a simple calculator that uses floats
-2. create the same calculator using command pattern
+1. create a **simple** calculator that uses **floats**. 
+
+**done***********************
+
+2. create the **same** calculator using **command pattern**
+    - a base class as the interface with an execute method with no body.
+    - concrete classes (add, subtract, multiply and divide) implement the interface. Consider interface methods as place holders.
+    - an invoker: a class with commands as intance attributes
+    - a method to set command
+    - a method to use command methods. 
+    - commands refer to concrete classes.
+
+    *************done**********************************
 3. modify second version to take and return fractions
