70fb223b556565f8f2fa7b4888cad3e735534142
zimula <zimula@live.dk>
2023-10-23 23:30:09 +0200

    GUI added to command pattern calculator

--- system udvikling-Pending/assignments/rational calculator/command pattern/float calculator.py
+++ system udvikling-Pending/assignments/rational calculator/command pattern/float calculator.py
@@ -1,3 +1,6 @@
+from graphics import *
+'Should separate these by module at later date. '
+
 'the interface'
 class Command:
     def execute(self, x, y):
@@ -38,24 +41,91 @@ class Calculator:
         
 'Main (should probably put everything in its own module) '
 def main():
+    'gui'
+    win = GraphWin("float calculator", 600, 600)
+    win.setBackground('orange')
+    x = win.getWidth()
+    y = win.getHeight()
+    intro = Text(Point(x-300, y -100 ), "powered by Command-Pattern")
+    intro.setFace("helvetica")
+    intro.setStyle("bold italic")
+    intro.setTextColor('orangered')
+    intro.setSize(30)
+    intro.draw(win)
+    
+    'input'
+    text1 = Text(Point(100,50), "First no.")
+    entry1 = Entry(Point(250, 50), 20)
+    text2 = Text(Point(100,100), "Second no.")
+    entry2 = Entry(Point(250, 100), 20)
+
+    text1.draw(win)
+    text2.draw(win)
+    entry1.draw(win)
+    entry2.draw(win)
+
+    'choosing operation'
+    opText = Text(Point(120, 150), "Select Operation")
+    opText.setStyle('bold')
+    opText.draw(win)
+
+    'buttons: loaded into list based on items in operations list'
+    opButtons = []
+    opSympbols = ['+','-','*','/']
+    for i, opSympbol in enumerate(opSympbols):
+        button = Rectangle(Point(60,180 + i*35),Point(200, 210+i*35))
+        buttonText = Text(button.getCenter(),opSympbol)
+        buttonText.setSize(20)
+        buttonText.setTextColor('green')
+        button.setFill('blue')
+        button.draw(win)
+        buttonText.draw(win)
+        'load button to opButtons'
+        opButtons.append((button, buttonText))
+
+    'output'
+    resultText = Text(Point(400,200),"ghgh")
+    resultText.setSize(25)
+    resultText.setStyle('bold')
+    resultText.draw(win)
+
+
+    'using invoker'
     calculator = Calculator()
     while True:
-        x = float(input("Enter x value: "))
-        y = float(input("Enter y value: "))
-        operation = input("Enter operation: ")
+        try:
+            'invent listener'
+            click_point = win.getMouse()
+            for button, buttonText in opButtons:
+                left, top = button.getP1().getX(), button.getP1().getY()
+                right, bottom = button.getP2().getX(), button.getP2().getY()
+                if left < click_point.getX() < right and top < click_point.getY() < bottom:
+                    selectedOp = buttonText.getText()
+                    opText.setText(f'{selectedOp}')
+            
+            'getting the values'
+            x = float(entry1.getText())
+            y = float(entry2.getText())
+        
+            
+            operation = selectedOp
 
-        if operation not in ['+', '-', '*', '/']:
+            if operation not in ['+', '-', '*', '/']:
+                break
+            elif operation == '+':
+                calculator.set_command(command_add())
+            elif operation == '-':
+                calculator.set_command(command_subtract())
+            elif operation == '*':
+                calculator.set_command(command_multiply())
+            elif operation == '/':
+                calculator.set_command(command_divide())
+            result = calculator.calculate(x,y)
+            resultText.setText(result)
+            print(f'Result: {result}')
+        except GraphicsError:
             break
-        elif operation == '+':
-            calculator.set_command(command_add())
-        elif operation == '-':
-            calculator.set_command(command_subtract())
-        elif operation == '*':
-            calculator.set_command(command_multiply())
-        elif operation == '/':
-            calculator.set_command(command_divide())
-        result = round(calculator.calculate(x,y),2)
-        print(result)
-        break
+    win.close()
+        
 if __name__ == '__main__':
     main()
--- system udvikling-Pending/assignments/rational calculator/readme.md
+++ system udvikling-Pending/assignments/rational calculator/readme.md
@@ -9,7 +9,18 @@
     - a method to set command
     - a method to use command methods. 
     - commands refer to concrete classes.
-
+    - later I will try to separate the code into modules. 
     *************done**********************************
+
 3. modify second version to take and return fractions
+    - requires fractions library
+
 4. might add **GUI**
+    - started
+    - use try and except to surpress graphical error from getmouse() when win is closed.
+    - must switch to **tkinter.** **graphics.py** is painful. 
+    ************************done******************************** 
+
+5. Rebuild in **html and js**
+    - use selenium to run tests on site.
+    - see it works with a local page.  
