ff7eac4246b5d2aa29ccf998703cce85000aef9b
zimula <zimula@live.dk>
2023-10-25 08:27:27 +0200

    rational calculator finished

--- system udvikling-Pending/assignments/rational calculator/command pattern/rational/concrete_commands.py
+++ system udvikling-Pending/assignments/rational calculator/command pattern/rational/concrete_commands.py
@@ -1,24 +1,27 @@
 from interfaces import ICommand
-from fractions import Fraction
+
 
 'my lcd finder'
 def lcd(x, y):
-    pass
+    return x.denominator * y.denominator
+
 
 
 'concrete commands to implement interface methods/ promises'
 class command_add(ICommand):
     def execute(self, x, y):
-        return x + y
+        return (lcd /x.numerator)*x.numerator + (lcd / y.numerator)*y.numerator
+        
 
 class command_subtract(ICommand):
     def  execute(self, x, y):
-        return x - y
+        return (lcd/x.numerator)*x.numerator + (y.numerator)*y.numerator
+        
 
 class command_multiply(ICommand):
     def execute(self, x, y):
-        return x * y
+        return (x.numerator * y.numerator)/(x.denominator * y.denominator)
 
 class command_divide(ICommand):
     def execute(self, x, y):
-        return x / y
+        return (x.numerator/x.denominator)*(y.denominator/ y.numerator)
